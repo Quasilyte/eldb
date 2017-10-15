e430811f0eb2584c5df4665aa0177aac8aae5932
quasilyte <quasilyte@gmail.com>
2017-10-15 20:35:17 +0300

    all: store data and metadata separately
    refs #2.

--- eldb.el
+++ eldb.el
@@ -18,102 +18,173 @@
 
 ;;; Code:
 
-;; To learn how exactly data is stored,
-;; see documentation that is available at `eldb' github repo.
-(defconst eldb--data-filename
-  (expand-file-name
-   "data.el"
-   (file-name-directory (buffer-file-name)))
-  "Database file name.")
+;; Unexported, compile-time only definitions.
+;; Makes compiled package cleaner,
+;; and private API is better encapsulated.
+(eval-when-compile
+  ;; `pkg' type is a pending package that is not indexed.
+  (defmacro eldb--make-pkg (code)
+    `(vector ,code))
+  (defmacro eldb--pkg-code (pkg)
+    `(aref ,pkg 0))
+  ;; `info' type is a package metadata unit.
+  (defmacro eldb--make-info (id)
+    `(vector ,id))
+  (defmacro eldb--info-id (info)
+    `(aref ,info 0))
+  ;; Utils.
+  (defmacro eldb--dohash (key-val-hash &rest body)
+    (declare (indent defun))
+    (let ((key (nth 0 key-val-hash))
+          (val (nth 1 key-val-hash))
+          (hash (nth 2 key-val-hash)))
+      `(maphash (lambda (,key ,val) ,@body) ,hash)))
+  )
+
+(defconst eldb--data-dir (file-name-directory (buffer-file-name))
+  "Data tarball directory.")
+
+(defconst eldb--info-filename
+  (format "%s/data/info.el" eldb--data-dir)
+  "Info data filename.")
+
+(defconst eldb--code-filename
+  (format "%s/data/code.el" eldb--data-dir)
+  "Code data filename.")
 
 (defconst eldb--max-context 128
   "Max query context length in chars.")
 
-(defvar eldb--data nil
-  "Database state object.")
+(defvar eldb--packages (make-hash-table :test 'eq)
+  "Map that contains freshly added packages.
+These can not be queried until `eldb-data-save' is called.")
+
+(defvar eldb--infodata nil
+  "Mapping of package symbols to `info' objects.")
+
+(defvar eldb--codedata nil
+  "Vector of `read'-able strings.
+Each string represents single package source code.")
 
 ;; Public:
 
 (defun eldb-data-load ()
-  "Load database file into Emacs, if it is not already loaded.
+  "Load database files into Emacs, if they are not already loaded.
 Should be called before any operations over corpus."
   (unless (eldb-data-loaded-p)
     (eldb-data-reload)))
 
 (defun eldb-data-reload ()
-  "Forced load of database file."
+  "Forced load of database files."
   (eldb--check-tar-bz)
-  (shell-command (format "tar -jxf %s.tar.bz2 -C %s"
-                         (file-name-nondirectory eldb--data-filename)
-                         (file-name-directory eldb--data-filename)))
-  (load-file eldb--data-filename))
+  (shell-command
+   (format "tar -jxf data.tar.bz2 -C \"%s\""
+           eldb--data-dir))
+  (mapc #'load-file
+        (list eldb--info-filename
+              eldb--code-filename)))
 
 (defun eldb-data-loaded-p ()
-  "Return non-nil if database file is loaded into Emacs."
-  (not (not eldb--data)))
+  "Return non-nil if database files are loaded into Emacs."
+  (not (not eldb--infodata)))
 
 (defun eldb-data-reset ()
-  "Reset database object state.  Last saved database file is left untouched.
+  "Reset database state objects.  Last saved database files are left untouched.
 To make changes permanent, call `eldb-data-save' after this."
-  (when (y-or-n-p "Reset database object?")
-    (setq eldb--data (make-hash-table :test 'eq))))
+  (when (y-or-n-p "Reset database objects?")
+    (setq eldb--packages (make-hash-table :test 'eq)
+          eldb--infodata (make-hash-table :test 'eq)
+          eldb--codedata [])))
 
 (defun eldb-data-save ()
-  "Write current database state to the filesystem."
+  "Write current database state to the filesystem.
+Also updates loaded data state: makes newly added packages
+available for queries."
   (eldb--check-tar-bz)
-  (when (and (eldb-data-loaded-p)
-             (not (hash-table-p eldb--data)))
-    (error "`eldb--data' is corrupted"))
-  (with-temp-file eldb--data-filename
-    ;; This code avoids big string allocation.
-    ;; `prin1' writes dirrectly to the buffer.
-    (insert "(defconst eldb--data ")
-    (prin1 eldb--data (current-buffer))
-    (insert ")"))
-  ;; Use `-C' to avoid full paths leaking to the archive.
-  (shell-command (format "tar -cjf %s.tar.bz2 %s -C %s"
-                         (file-name-nondirectory eldb--data-filename)
-                         (file-name-nondirectory eldb--data-filename)
-                         (file-name-directory eldb--data-filename))))
+  (let ((infodata (make-hash-table :test 'eq))
+        (codedata [])
+        (code-list nil)
+        (id 0))
+    ;; Collect new data.
+    (eldb--dohash (sym pkg eldb--packages)
+      (puthash sym
+               (eldb--make-info id)
+               infodata)
+      (push (eldb--pkg-code pkg) code-list)
+      (setq id (1+ id)))
+    ;; Merge old data.
+    (eldb--dohash (sym info eldb--infodata)
+      (unless (gethash sym infodata)
+        (puthash sym
+                 (eldb--make-info id)
+                 infodata)
+        (push (aref eldb--codedata (eldb--info-id info)) code-list)
+        (setq id (1+ id))))
+    ;; Prepare data.
+    (setq codedata (vconcat (nreverse code-list)))
+    ;; Write data files.
+    (with-temp-file eldb--info-filename
+      (insert "(setq eldb--infodata ")
+      (prin1 infodata (current-buffer))
+      (insert ")"))
+    (with-temp-file eldb--code-filename
+      (insert "(setq eldb--codedata ")
+      (prin1 codedata (current-buffer))
+      (insert ")"))
+    ;; Combine data files into tarball.
+    (shell-command
+     (format "tar -cjf data.tar.bz2 data/info.el data/code.el -C \"%s\""
+             eldb--data-dir))
+    ;; Update state variables.
+    (setq eldb--packages (make-hash-table :test 'eq)
+          eldb--infodata infodata
+          eldb--codedata codedata)))
 
-(defun eldb-insert-forms (pkg forms)
-  "Associate PKG symbol with FORMS.
+(defun eldb-insert-forms (sym forms)
+  "Associate SYM with FORMS.
 FORMS must be a list of package forms with `progn' head.
-For example, '(progn (list 1 2) (+ 1 2)) is valid."
-  (unless (symbolp pkg)
-    (error "PKG must be a symbol, %S given" (type-of pkg)))
+For example, '(progn (list 1 2) (+ 1 2)) is valid.
+
+Packages that were inserted, but not written to
+filesystem yet are not available for querying.
+To flush pending packages, call `eldb-data-save'."
+  (unless (symbolp sym)
+    (error "SYM must be a symbol, %S given" (type-of sym)))
   (unless (listp forms)
     (error "FORMS must be a list, %S given" (type-of forms)))
   (unless (eq 'progn (car forms))
     (error "FORMS head should be `progn', got %S" (car forms)))
-  (let ((text (eldb--minify-text
+  (let ((code (eldb--minify-code
                (prin1-to-string
                 (eldb--minify-forms forms)))))
     ;; Add space padding at the both string sides.
     ;; This makes taking a substring with context pad simpler
     ;; as there is no corner cases.
-    (setq text (concat (make-string eldb--max-context ? )
-                       text
+    (setq code (concat (make-string eldb--max-context ? )
+                       code
                        (make-string eldb--max-context ? )))
-    (puthash pkg text eldb--data)))
+    (puthash sym
+             (eldb--make-pkg code)
+             eldb--packages)))
 
-(defun eldb-insert-text (pkg text)
-  "Associate PKG symbol with TEXT.
-TEXT may have multiple Emacs Lisp forms.
-Result will be wrapped in `progn' form."
-  (unless (stringp text)
-    (error "TEXT must be string, %S given" (type-of text)))
+(defun eldb-insert-code (sym code)
+  "Associate SYM with CODE.
+CODE may have multiple Emacs Lisp forms.
+Result will be wrapped in `progn' form.
+See `eldb-insert-forms' for additional details."
+  (unless (stringp code)
+    (error "CODE must be string, %S given" (type-of code)))
   ;; Newlines are needed to avoid issues with leading
   ;; and trailing comments.
-  (eldb-insert-forms pkg (read (format "(progn \n%s\n)" text))))
+  (eldb-insert-forms sym (read (format "(progn \n%s\n)" code))))
 
 (defun eldb-insert-files (pkg files)
   "Associate PKG symbol with text collected from FILES.
-Concatenates file contents and calls `elds-insert-text'."
+Concatenates file contents and calls `elds-insert-code'."
   (dolist (f files)
     (unless (file-exists-p f)
       (error "file `%s' does not exist" f)))
-  (eldb-insert-text pkg
+  (eldb-insert-code pkg
                     (with-temp-buffer
                       (dolist (f files)
                         (insert-file-contents f)
@@ -125,15 +196,12 @@ Concatenates file contents and calls `elds-insert-text'."
 Convenience wrapper around `eldb-insert-files' for single file."
   (eldb-insert-files pkg (list file)))
 
-(defun eldb-query-package (pkg-regexp regexp &optional context)
-  "For packages that match PKG-REGEXP, collect REGEXP matches with CONTEXT pad."
+(defun eldb-query-corpus (regexp &optional context)
+  "Collect REGEXP matches with CONTEXT pad.  Scans entire corpus."
   (let ((matches nil))
-    (maphash
-     (lambda (pkg text)
-       (when (string-match-p pkg-regexp (symbol-name pkg))
-         (push (eldb--query-text text regexp context)
-               matches)))
-     eldb--data)
+    (dotimes (i (length eldb--codedata))
+      (push (eldb--query-code (aref eldb--codedata i) regexp context)
+            matches))
     (apply 'append matches)))
 
 ;; Private:
@@ -144,11 +212,11 @@ Convenience wrapper around `eldb-insert-files' for single file."
 Transformations are defined in `eldb' package description."
   forms)
 
-(defun eldb--minify-text (text)
-  "Apply compressions to TEXT.
+(defun eldb--minify-code (code)
+  "Apply compressions to CODE.
 Transformations are defined in `eldb' package description."
   ;; Escape newlines.
-  (replace-regexp-in-string "\n" "\\n" text nil t))
+  (replace-regexp-in-string "\n" "\\n" code nil t))
 
 (defsubst eldb--context-size (context)
   "Return corrected CONTEXT argument."
@@ -157,14 +225,14 @@ Transformations are defined in `eldb' package description."
     ;; Default value.
     20))
 
-(defun eldb--query-text (text regexp &optional context)
-  "Collect lines from TEXT that match REGEXP with CONTEXT pad.
-TEXT is expected to be package source text."
+(defun eldb--query-code (code regexp &optional context)
+  "Collect lines from CODE that match REGEXP with CONTEXT pad.
+CODE is expected to be package source text."
   (setq context (eldb--context-size context))
   (let ((pos 0)
         (matches nil))
-    (while (setq pos (string-match regexp text pos))
-      (push (substring text
+    (while (setq pos (string-match regexp code pos))
+      (push (substring code
                        (- pos context)
                        (+ (match-end 0) context))
             matches)
